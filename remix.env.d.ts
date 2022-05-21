b4d0dddc1043940ddb3e8a2aac8c8649ae3b7665
Karl Edenstål <karl.edenstal@rhyme.se>
2022-05-21 23:12:11 +0200

    env d

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node/globals" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production" | "test";
+    API_URL: string;
+  }
+}
